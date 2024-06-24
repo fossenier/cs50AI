ec838fae43a215d49afb7742bb381895b06f178c
Logan Fossenier <logan.fossenier@gmail.com>
2024-06-24 15:33:40 -0600

    feat: discover nice system for solving the problems

--- knowledge/knights/puzzle.py
+++ knowledge/knights/puzzle.py
@@ -9,38 +9,73 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-# Each speaker is either a knight or a knave
-game_rules = And(
-    Biconditional(AKnight, Not(AKnave)),
-    Biconditional(BKnight, Not(BKnave)),
-    Biconditional(CKnight, Not(CKnave)),
-)
-
 # Puzzle 0
 # A says "I am both a knight and a knave."
 statement_A0 = And(
     AKnave,
     AKnight,
 )
-
 knowledge0 = And(
-    game_rules,
-    # Truthfulness of statement implies knighthood
+    # A is a knight or a knave.
+    Or(AKnight, AKnave),
+    # A is not both a knight and a knave.
+    Not(And(AKnight, AKnave)),
+    # Telling the truth leads to being a knight. Being a knight leads to lying.
     Biconditional(statement_A0, AKnight),
+    # Lying leads to being a knave. Being a knave leads to lying.
+    Biconditional(Not(statement_A0), AKnave),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
+statement_A1 = And(
+    AKnave,
+    BKnave,
+)
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A is a knight or a knave.
+    Or(AKnight, AKnave),
+    # A is not both a knight and a knave.
+    Not(And(AKnight, AKnave)),
+    # Telling the truth leads to being a knight. Being a knight leads to lying.
+    Biconditional(statement_A1, AKnight),
+    # Lying leads to being a knave. Being a knave leads to lying.
+    Biconditional(Not(statement_A1), AKnave),
+    # B is a knight or a knave.
+    Or(BKnight, BKnave),
+    # B is not both a knight and a knave.
+    Not(And(BKnight, BKnave)),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
+statement_A2 = And(
+    Biconditional(AKnight, BKnight),
+    Biconditional(AKnave, BKnave),
+)
 # B says "We are of different kinds."
+statement_B2 = And(
+    Biconditional(AKnight, BKnave),
+    Biconditional(AKnave, BKnight),
+)
 knowledge2 = And(
-    # TODO
+    # A is a knight or a knave.
+    Or(AKnight, AKnave),
+    # A is not both a knight and a knave.
+    Not(And(AKnight, AKnave)),
+    # Telling the truth leads to being a knight. Being a knight leads to lying.
+    Biconditional(statement_A2, AKnight),
+    # Lying leads to being a knave. Being a knave leads to lying.
+    Biconditional(Not(statement_A2), AKnave),
+    # B is a knight or a knave.
+    Or(BKnight, BKnave),
+    # B is not both a knight and a knave.
+    Not(And(BKnight, BKnave)),
+    # Telling the truth leads to being a knight. Being a knight leads to lying.
+    Biconditional(statement_B2, BKnight),
+    # Lying leads to being a knave. Being a knave leads to lying.
+    Biconditional(Not(statement_B2), BKnave),
 )
 
 # Puzzle 3
